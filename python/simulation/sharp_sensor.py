08a8b37b00baf795f9ee06f2f53f7ecec0ae2b20
Mattis Marjak <mattis.marjak@defendec.com>
2014-11-29 02:17:14 +0200

    add sharp 150cm sensor lookup table generation

--- python/simulation/sharp_sensor.py
+++ python/simulation/sharp_sensor.py
@@ -6,7 +6,27 @@ class SharpSensor(object):
     # values imported from
     # http://www.sharpsme.com/download/gp2y0a21yk-epdf
     # by converting pdf to svg with inkscape
-    distance_to_voltage_characteristic = [
+    distance_to_voltage_characteristic_150cm = [
+        # (0.00, 0.00),
+        # (10.49, 231.65),
+        (15.53, 275.00),
+        (20.76, 251.80),
+        (29.99, 199.09),
+        (40.00, 152.95),
+        (50.00, 122.86),
+        (60.00, 103.26),
+        (70.00, 88.40),
+        (79.99, 80.08),
+        (90.54, 69.57),
+        (100.00, 62.95),
+        (110.50, 56.88),
+        (120.00, 51.91),
+        (129.99, 48.15),
+        (139.99, 43.62),
+        (150.00, 40.58),
+    ]
+
+    distance_to_voltage_characteristic_80cm = [
         (   0.00,    0.00),
         (  87.33,  467.44),
         ( 121.28,  690.47),
@@ -33,15 +53,19 @@ class SharpSensor(object):
         (7020.02,  441.47),
         (8000.00,  412.45),
     ]
-    x,y = map(np.array, zip(*distance_to_voltage_characteristic))
+    x,y = map(np.array, zip(*distance_to_voltage_characteristic_150cm))
     distance_to_voltage_polyline = PiecewisePolynomial(
         x, y[:,np.newaxis]
+    )
+    x1,y1 = map(np.array, zip(*distance_to_voltage_characteristic_150cm[::-1]))
+    voltage_to_distance_polyline = PiecewisePolynomial(
+        y1, x1[:,np.newaxis]
     )
 
     @classmethod
     def plot(cls):
         fig, ax = plt.subplots()
-        ax.plot(cls.x, cls.y)
+        ax.plot(cls.y, cls.x)
         ax.set_ylabel("Output voltage (mV)")
         ax.set_xlabel("Distance to reflective object L(mm)")
         plt.show()
@@ -55,6 +79,26 @@ class SharpSensor(object):
         # TODO: Implement distance_to_sample
         raise NotImplemented
 
+    @classmethod
+    def create_vol_to_distance_lut(cls):
+        sample_count = 1024
+        voltage_step = 330./sample_count
+        for i, vol in enumerate([voltage_step*x for x in range(1024)]):
+            if i % 16 == 0:
+                print ""
+            if  vol > max(cls.y):
+                vol = max(cls.y)
+            elif vol < min(cls.y):
+                vol = min(cls.y)
+            print "{:>3.0f},".format(cls.voltage_to_distance_polyline((vol,))[0]),
+
+
 if __name__ == "__main__":
-    print(SharpSensor.distance_to_voltage(130, 400, 460.86))
-    # ss.plot()
+    # print(SharpSensor.distance_to_voltage(130, 400, 460.86))
+
+    SharpSensor.create_vol_to_distance_lut()
+    # SharpSensor.plot()
+    # print(SharpSensor.x)
+    # print(SharpSensor.y)
+    # for x,y in SharpSensor.distance_to_voltage_characteristic_150cm:
+    #     print("({:>4.2f}, {:>4.2f}),".format(x*150/133.23, y*275/121.36))
